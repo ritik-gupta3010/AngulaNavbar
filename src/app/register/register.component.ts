ae5710b795e61e9d495f7a976076339dd981d395
Ritik Gupta <ritikgupta30102000@gmail.com>
2023-04-13 15:16:59 -0700

    validation remaininf input

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,15 +8,45 @@ import { Validators, FormGroup, FormControl } from '@angular/forms';
 export class RegisterComponent {
   title = "Angular Reactive form"
   registerForm = new FormGroup({
-    fName:new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(50)]),
-    lName:new FormControl('',[Validators.required]),
-    eMail:new FormControl('',[Validators.email]),
-    mobileNumber:new FormControl('',[Validators.required]),
-    gender:new FormControl('',[Validators.required])
+    fName:new FormControl('',[Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(10),
+      Validators.pattern("[a-zA-Z]*")
+    ]),
+    lName:new FormControl('',[Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(50),
+      Validators.pattern("[a-zA-Z]*")
+    ]),
+    eMail:new FormControl('',[Validators.required,
+      Validators.email]),
+    mobileNumber:new FormControl('',[Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(10),
+      Validators.pattern("[6-9][0-9]*")
+    ]),
+    gender:new FormControl('',[Validators.required]),
+    passWord:new FormControl('',[Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(15),
+      Validators.pattern("")
+    ]),
+    repeatPassword:new FormControl('')
   })
+  repeatPassCSS: String="none"
   registerUser()
   {
-    console.log(this.registerForm.value)
+    console.log(this.registerForm.get("passWord")?.value)
+    console.log(this.registerForm.get("repeatPassword")?.value)
+    if(this.registerForm.get("passWord")?.value == this.registerForm.get("repeatPassword")?.value)
+    {
+      alert("submitted");
+      console.log(this.registerForm.value)
+    }
+    else{
+      this.repeatPassCSS="inline"
+      console.log(this.registerForm.value)
+    }
   }
   get fName()
   {
@@ -41,5 +71,13 @@ export class RegisterComponent {
   get gender()
   {
     return this.registerForm.get("gender");
+  }
+  get passWord()
+  {
+    return this.registerForm.get("passWord");
+  }
+  get repeatPassword()
+  {
+    return this.registerForm.get("repeatPassword");
   }
 }
